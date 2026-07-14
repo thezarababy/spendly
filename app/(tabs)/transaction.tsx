cf2d6ed49c6cfbb23f054b82a85a5693842064de
thezarababy <jyokoduwa@gmail.com>
2026-07-14 03:39:49 +0100

    Refactor project structure and remove unused components; add initial data setup

---
+++ app/(tabs)/transaction.tsx
@@ -0,0 +1,4 @@
+export default function Transaction(){
+    return null
+
+}
---
+++ data.ts
@@ -0,0 +1,26 @@
+[
+  {
+    id: "1",
+    title: "Salary",
+    amount: 250000,
+    type: "income",
+    category: "Salary",
+    date: "2026-07-14",
+  },
+  {
+    id: "2",
+    title: "Groceries",
+    amount: 15000,
+    type: "expense",
+    category: "Food",
+    date: "2026-07-13",
+  },
+  {
+    id: "3",
+    title: "Transport",
+    amount: 5000,
+    type: "expense",
+    category: "Transport",
+    date: "2026-07-12",
+  }
+]
