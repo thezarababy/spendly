c3caf5ac9eb8eee46a12a440b3a91dfcf78fc49d
thezarababy <jyokoduwa@gmail.com>
2026-07-14 15:06:47 +0100

    Add typography and splash screen handling; update dependencies for font support-Configure Inter fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,20 @@
-
+import React from 'react';
+import { Text } from 'react-native';
+import {typography} from '@/constants/typography';
 
 export default function HomeScreen() {
-  return null
-    
+  return (
+    <>
+      <Text style={{ fontFamily: typography.fontFamily.bold, fontSize: typography.fontSize["4xl"] }}>
+        Spendly
+      </Text>
+
+      <Text style={{
+    fontFamily: typography.fontFamily.medium,
+    fontSize: typography.fontSize.lg,
+  }}>
+        Welcome to Spendly!
+      </Text>
+    </>
+  );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,37 @@
-
+import {Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold} from '@expo-google-fonts/inter';
+import {useFonts} from 'expo-font';
 import { Stack } from 'expo-router';
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+  });
+
+   useEffect(() => {
+    if (fontsLoaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
   
+
 
   return (
     <Stack screenOptions={{headerShown:false}}>
 <Stack.Screen name='(tabs)'/>
 <Stack.Screen name='addTransaction'/>
+
+
     </Stack>
     
   );
