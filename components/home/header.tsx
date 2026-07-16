0ef8289fe908a73a16629e3b3815875430ac6e9c
thezarababy <jyokoduwa@gmail.com>
2026-07-16 17:25:54 +0100

    Implement Header component with greeting and summary text; replace placeholder with styled View

--- components/home/header.tsx
+++ components/home/header.tsx
@@ -1,5 +1,23 @@
+import { spacing } from "@/constants";
 import React from "react";
+import { StyleSheet, View } from "react-native";
+import AppText from "../ui/AppText";
 
 export default function Header() {
-  return <div> This is the header</div>;
+  const greeting = "Good Morning";
+  const userName = "Joy";
+  return (
+    <View style={styles.container}>
+      <AppText variant="heading">
+        {greeting} {userName} 👋
+      </AppText>
+      <AppText variant="body">Here's your summary</AppText>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: spacing.md,
+  },
+});
